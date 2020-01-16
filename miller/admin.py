9bb492cb8f6c62af4bb55c82c275f5078f8a526a
Daniele Guido <gui.daniele@gmail.com>
2020-01-16 16:46:23 +0100

    add models to the admin

--- miller/admin.py
+++ miller/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Story, Tag, Document, Caption, Mention, Author
+
+admin.site.register(Story)
+admin.site.register(Tag)
+admin.site.register(Document)
+admin.site.register(Caption)
+admin.site.register(Mention)
+admin.site.register(Author)
