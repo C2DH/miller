76b27f0a93f61ed11d5c07fa0e6d2101aa7b1cc6
frederic.reis <frederic.reis@uni.lu>
2020-03-24 19:18:10 +0100

    issue #32: Add fields in the document form to allow editing each property of the data JSON field
    * Generate the dynamic form from the schema properties

--- miller/admin.py
+++ miller/admin.py
@@ -59,6 +59,12 @@ class DataAdminForm(forms.ModelForm):
 class DocumentAdmin(admin.ModelAdmin):
     list_display = ('id','slug','title','type')
     list_filter = ('type', DataTypeListFilter, DataProviderListFilter)
+    fieldsets = [
+        (None, {'fields': ['type', 'short_url', 'title', 'slug']}),
+        ('Metadata', {'fields': ['data']}),
+        ('Content', {'fields': ['copyrights', 'url', 'owner', 'attachment', 'snapshot', 'mimetype', 'locked', 'search_vector', 'documents']})
+    ]
+#    readonly_fields = ['data']
     form = DataAdminForm
     change_form_template = 'miller/document/document_change_form.html'
 
--- miller/static/js/edit_json_field.js
+++ miller/static/js/edit_json_field.js
@@ -1,6 +1,31 @@
 (function($) {
 
+  /** Selectors */
   var DEFAULT_JSON_FIELD_SELECTOR = 'textarea';
+
+  /** Attributes */
+  var ID = 'id';
+  var NAME = 'name';
+  var FOR = 'for';
+
+  /** HTML tags */
+  var LABEL_TAG = 'label';
+
+  /** HTML */
+  var FIELD_CONTAINER_HTML  = '\
+    <div class="form-row">\
+      <div>\
+        <label></label>\
+      </div>\
+    </div>\
+  ';
+  var INPUT_FIELD_HTML = '\
+    <input type="text" class="vTextField" maxlength="500"></input>\
+  ';
+  var HELP_TEXT_HTML = '\
+    <div class="help"></div>\
+  ';
+
 
   /**
    * Construction of EditJSONField
@@ -18,6 +43,10 @@
 
     this.config     = config || {};
     this.rootEl     = $(rootEl);
+    if(this.rootEl.length == 0) {
+      console.error("No root element specified!")
+      return;
+    }
 
     //  Determine the JSON data field
     this.jsonField  = $(this.config.jsonField);
@@ -69,9 +98,66 @@
   App.prototype._init = function(schema) {
 
     this.schema = schema;
+    this.field_id_pfx = this.jsonField.attr('id') + '_';
 
-    console.log(this.schema);
+    //  Get JSON data
+    this.jsonData = $.parseJSON(this.jsonField.text());
 
+    for(var fieldId in this.schema.properties) {
+      this._addField(fieldId, this.schema.properties[fieldId]);
+    }
+
+    this._updateJSONData();
+  }
+
+
+  /**
+   * Add a field to edit the JSON property identified by the fieldId parameter
+   *
+   * @param fieldId   id of the json property to edit
+   * @param fieldProperties   properties of the json property to edit
+   *
+   * @author  fre
+   * @since   March 24, 2020
+   */
+  App.prototype._addField = function(fieldId, fieldProperties) {
+
+    this.jsonData[fieldId] = this.jsonData[fieldId] || "";
+
+    //  Add the field container with the label
+    var field = $(FIELD_CONTAINER_HTML);
+    var label = field.find(LABEL_TAG);
+    label
+      .attr(FOR, this.field_id_pfx + fieldId)
+      .text(fieldProperties.title);
+
+    //  Add the input field
+    var inputField = $(INPUT_FIELD_HTML);
+    inputField
+      .attr(ID, this.field_id_pfx + fieldId)
+      .attr(NAME, fieldId)
+      .val(this.jsonData[fieldId])
+      .insertAfter(label);
+
+    //  Addd the help text
+    if(fieldProperties.description)
+      $(HELP_TEXT_HTML)
+        .text(fieldProperties.description)
+        .insertAfter(inputField)
+
+    this.rootEl.append(field);
+
+  }
+
+
+  /**
+   * Update the data on the JSON field
+   *
+   * @author  fre
+   * @since   March 24, 2020
+   */
+  App.prototype._updateJSONData = function() {
+    this.jsonField.text(JSON.stringify(this.jsonData));
   }
 
 
