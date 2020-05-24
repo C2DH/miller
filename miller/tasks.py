3457a02df81be13bd66f63a6b56cd3dd08208f5a
Daniele Guido <gui.daniele@gmail.com>
2020-05-24 19:27:28 +0200

    add celery task and create a management command for it

--- miller/tasks.py
+++ miller/tasks.py
@@ -1,5 +1,6 @@
 from .celery import app
 from celery.utils.log import get_task_logger
+from .models import Document
 
 logger = get_task_logger(__name__)
 
@@ -10,3 +11,14 @@ def echo(self, message):
 @app.task(bind=True)
 def populate_search_vectors(self, story_id):
     logger.info('story_id: {}'.format(story_id))
+
+@app.task(bind=True, autoretry_for=(Exception,), exponential_backoff=2, retry_kwargs={'max_retries': 5}, retry_jitter=True)
+def create_document_snapshot(self, document_pk):
+    logger.info('document_pk: {}'.format(document_pk))
+    doc = Document.objects.get(pk=document_pk)
+    doc.create_snapshot_from_attachment()
+
+@app.task(bind=True, autoretry_for=(Exception,), exponential_backoff=2, retry_kwargs={'max_retries': 5}, retry_jitter=True)
+def create_document_snapshot_images(self, document_pk):
+    logger.info('document_pk: {}'.format(document_pk))
+    doc = Document.objects.get(pk=document_pk)
