aaf197c0b1bd8fa93832603d122420cfc9b36654
Daniele Guido <gui.daniele@gmail.com>
2020-05-25 15:52:19 +0200

    better logging og JSON errors

--- miller/utils/schema.py
+++ miller/utils/schema.py
@@ -1,23 +1,32 @@
-import os, json, logging
+import os
+import json
+import logging
 
 from django.conf import settings
 from jsonschema import validate as jsonschemaValidate
 
 logger = logging.getLogger(__name__)
 
+
 class JSONSchema:
     def __init__(self, filepath):
         abs_filepath = os.path.join(settings.MILLER_SCHEMA_ROOT, filepath)
-        logger.info('JSONSchema() init on abs_filepath:\'{}\''.format(
-            settings.MILLER_SCHEMA_ROOT,
-            abs_filepath
-        ))
+        logger.info(
+            F'JSONSchema() init on abs_filepath:"{abs_filepath}"'
+            F'(MILLER_SCHEMA_ROOT: {settings.MILLER_SCHEMA_ROOT})'
+        )
         if settings.MILLER_SCHEMA_ENABLE_VALIDATION:
             with open(abs_filepath, 'r') as schema:
-                self.schema = json.load(schema)
-                self.filepath = filepath
+                try:
+                    self.schema = json.load(schema)
+                    self.filepath = filepath
+                except Exception as e:
+                    logger.error(F'Unable to load JSON from {abs_filepath}')
+                    raise e
         else:
-            logger.warning('settings.MILLER_SCHEMA_ENABLE_VALIDATION is disabled!')
+            logger.warning(
+                'settings.MILLER_SCHEMA_ENABLE_VALIDATION is disabled!'
+            )
 
     def validate(self, instance):
         logger.info('validate() using json filepath: {}'.format(self.filepath))
