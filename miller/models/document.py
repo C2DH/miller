b0925e3545baa4903c595959601fa4f8c0ddaad4
Daniele Guido <gui.daniele@gmail.com>
2020-05-26 11:48:26 +0200

    Update document.py

--- miller/models/document.py
+++ miller/models/document.py
@@ -1,6 +1,5 @@
 import os
 import logging
-import dpath.util
 
 from django.conf import settings
 from django.db import models
@@ -27,6 +26,7 @@ def snapshot_attachment_file_name(instance, filename):
 
 
 class Document(models.Model):
+    TBD = 'to be defined'
     BIBLIOGRAPHIC_REFERENCE = 'bibtex'
     CROSSREF_REFERENCE = 'crossref'
     VIDEO_COVER = 'video-cover'
@@ -44,6 +44,7 @@ class Document(models.Model):
     ENTITY = 'entity'
 
     TYPE_CHOICES = (
+        (TBD, 'to be defined'),
         (BIBLIOGRAPHIC_REFERENCE, 'bibtex'),
         (CROSSREF_REFERENCE, 'bibtex'),
         (VIDEO_COVER, 'video interview'),
@@ -56,35 +57,54 @@ class Document(models.Model):
         (RICH, 'rich'),
         (LINK, 'link'),
         (AV, 'audiovisual'),
-        (ENTITY, 'entity: see data type property'), # use the type field inside data JsonField.
+        # for ENTITY, use the type field inside data JsonField.
+        (ENTITY, 'entity: see data type property'),
     ) + settings.MILLER_DOCUMENT_TYPE_CHOICES
 
-    type       = models.CharField(max_length=24, choices=TYPE_CHOICES)
-    short_url  = models.CharField(max_length=22, db_index=True, unique=True, blank=True)
+    type = models.CharField(max_length=24, choices=TYPE_CHOICES, default=TBD)
+    short_url = models.CharField(
+        max_length=22, db_index=True, unique=True, blank=True
+    )
 
-    title      = models.CharField(max_length=500)
-    slug       = models.CharField(max_length=150, unique=True, blank=True, db_index=True)
+    title = models.CharField(max_length=500, default='')
+    slug = models.CharField(
+        max_length=150, unique=True, blank=True, db_index=True
+    )
 
-    # contents   = models.TextField(null=True, blank=True, default=json.dumps(DEFAULT_OEMBED, indent=1)) # OEMBED (JSON) metadata field, in different languages if available.
-
-    data       = UTF8JSONField(verbose_name=u'data contents', help_text='JSON format', default=dict, blank=True)
+    data = UTF8JSONField(
+        verbose_name=u'data contents', help_text='JSON format',
+        default=dict, blank=True
+    )
 
     copyrights = models.TextField(null=True, blank=True,  default='')
 
-    url        = models.URLField(max_length=500, null=True, blank=True)
-    owner      = models.ForeignKey(User, on_delete=models.CASCADE); # at least the first author, the one who owns the file.
-    attachment = models.FileField(upload_to=attachment_file_name, null=True, blank=True, max_length=200)
-    snapshot   = models.FileField(upload_to=snapshot_attachment_file_name, null=True, blank=True, max_length=200)
-    mimetype   = models.CharField(max_length=127, blank=True, default='')
+    url = models.URLField(max_length=500, null=True, blank=True)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        null=True, blank=True
+    )
+    attachment = models.FileField(
+        upload_to=attachment_file_name,
+        null=True, blank=True, max_length=200
+    )
+    snapshot = models.FileField(
+        upload_to=snapshot_attachment_file_name,
+        null=True, blank=True, max_length=200
+    )
+    mimetype = models.CharField(max_length=127, blank=True, default='')
 
-    locked     = models.BooleanField(default=False) # prevent accidental override when it is not needed.
+    # @TODO prevent accidental override when it is not needed.
+    locked = models.BooleanField(default=False)
 
     # add search field
     search_vector = SearchVectorField(null=True, blank=True)
 
     # add last modified date
-    # undirected
-    documents  = models.ManyToManyField("self", blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_last_modified = models.DateTimeField(auto_now=True)
+
+    # undirected links
+    documents = models.ManyToManyField("self", blank=True)
 
     def __str__(self):
         return self.slug
@@ -94,23 +114,28 @@ class Document(models.Model):
         if there is an attachment, generate a PNG snapshot or similar.
         If snapshot is already present, look for override param
         """
-        logger.debug('document pk:{} create_snapshot_from_attachment using type:{} ...'.format(
-            self.pk,
-            self.type,
-        ))
+        logger.debug(
+            f'create_snapshot_from_attachment document pk:{self.pk}'
+            f' using type:{self.type} ...'
+        )
 
         if not self.attachment or not getattr(self.attachment, 'path', None):
-            logger.error('document pk:{} no attachment found! Skip.'.format(self.pk))
+            logger.error(
+                f'create_snapshot_from_attachment document pk:{self.pk}'
+                f' failed, no attachment found! Skip.')
             return
 
         if not os.path.exists(self.attachment.path):
-            logger.error('document pk:{} snapshot cannot be generated, attached file {} does not exist.'.format(
-                self.pk,
-                self.attachment.path,
-            ))
+            logger.error(
+                f'create_snapshot_from_attachment document pk:{self.pk}'
+                ' failed, attached file {self.attachment.path} does not exist.'
+            )
             return
         # get snaphot path and its width / height
-        snapshot,w,h = create_snapshot(basepath=self.type, source=self.attachment.path)
+        snapshot, w, h = create_snapshot(
+            basepath=self.type,
+            source=self.attachment.path
+        )
         # save document
         self.snapshot = snapshot
         self.data.update({
