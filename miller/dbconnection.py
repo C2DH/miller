a0ee0388b4b3fb0ffe6640676a7e9a07c30fe208
Daniele Guido <gui.daniele@gmail.com>
2020-05-28 15:18:55 +0200

    Update dbconnection.py
    use env variables so that script is standalone

--- miller/dbconnection.py
+++ miller/dbconnection.py
@@ -1,22 +1,20 @@
+import os
 import sys
 import psycopg2
-from . import settings
-
-db_params = settings.DATABASES['default']
 
 try:
     connection = psycopg2.connect(
-        user=db_params['USER'],
-        password=db_params['PASSWORD'],
-        host=db_params['HOST'],
-        port=db_params['PORT'],
-        database=db_params['NAME']
+        user=os.environ.get('MILLER_DATABASE_USER'),
+        password=os.environ.get('MILLER_DATABASE_PASSWORD'),
+        host=os.environ.get('MILLER_DATABASE_HOST'),
+        port=os.environ.get('MILLER_DATABASE_PORT'),
+        database=os.environ.get('MILLER_DATABASE_NAME')
     )
 
     cursor = connection.cursor()
     cursor.execute("SELECT version();")
     record = cursor.fetchone()
-    print(f'Connected to: {record}')
+    print(f'dbconection.py connected to: {record}')
     sys.exit(0)
 except Exception as e:
     print('DB is not ready', e)
