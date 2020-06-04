fc23667c1b68270434793b4af9e11d2076f15f80
Daniele Guido <gui.daniele@gmail.com>
2020-06-04 10:59:53 +0200

    eslint

--- miller/models/tag.py
+++ miller/models/tag.py
@@ -2,43 +2,48 @@ from django.conf import settings
 from django.contrib.postgres.fields import JSONField
 from django.db import models
 
+
 class Tag(models.Model):
-  # categories
-  KEYWORD = 'keyword' # i.e, no special category at all
-  BLOG   = 'blog' # items tagged as events are "news"
-  HIGHLIGHTS   = 'highlights'
-  WRITING      = 'writing'
-  COLLECTION   = 'collection'
-  PUBLISHING   = 'publishing' # things related to publishing activity, I.E issue number that can be filtered by
+    KEYWORD = 'keyword'  # i.e, no special category at all
+    BLOG = 'blog'  # items tagged as events are "news"
+    HIGHLIGHTS = 'highlights'
+    WRITING = 'writing'
+    COLLECTION = 'collection'
+    # things related to publishing activity,
+    # I.E issue number that can be filtered by
+    PUBLISHING = 'publishing'
 
-  CATEGORY_CHOICES = (
-    (KEYWORD, 'keyword'),
-    (BLOG, 'blog'),
-    (HIGHLIGHTS, 'highlights'),
-    (WRITING, 'writing'),
-    (COLLECTION, 'collection'),
-    (PUBLISHING, 'publishing')
-  ) + settings.MILLER_TAG_CATEGORY_CHOICES
+    CATEGORY_CHOICES = (
+        (KEYWORD, 'keyword'),
+        (BLOG, 'blog'),
+        (HIGHLIGHTS, 'highlights'),
+        (WRITING, 'writing'),
+        (COLLECTION, 'collection'),
+        (PUBLISHING, 'publishing')
+    ) + settings.MILLER_TAG_CATEGORY_CHOICES
 
-  HIDDEN  = 'hidden'
-  PUBLIC  = 'public' # everyone can access that.
+    HIDDEN = 'hidden'
+    # everyone can access that.
+    PUBLIC = 'public'
 
-  STATUS_CHOICES = (
-    (HIDDEN, 'keep this hidden'),
-    (PUBLIC, 'published tag'),
-  )
+    STATUS_CHOICES = (
+        (HIDDEN, 'keep this hidden'),
+        (PUBLIC, 'published tag'),
+    )
+    # e.g. 'Mr. E. Smith'
+    name = models.CharField(max_length=100)
+    # e.g. 'mr-e-smith'
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
+    # e.g. 'actor' or 'institution'
+    category = models.CharField(
+        max_length=32, choices=CATEGORY_CHOICES, default=KEYWORD)
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default=PUBLIC)
 
-  name       = models.CharField(max_length=100) # e.g. 'Mr. E. Smith'
-  slug       = models.SlugField(max_length=100, unique=True, blank=True) # e.g. 'mr-e-smith'
-  category   = models.CharField(max_length=32, choices=CATEGORY_CHOICES, default=KEYWORD) # e.g. 'actor' or 'institution'
-  status     = models.CharField(max_length=10, choices=STATUS_CHOICES, default=PUBLIC)
+    data = JSONField(default=dict)
 
-  data       = JSONField(default=dict)
+    class Meta:
+        unique_together = ('name', 'category')
 
-  # search     = models.TextField(null=True, blank=True)# SearchVectorField(null=True, blank=True) # index search
-
-  class Meta:
-    unique_together = ('name', 'category')
-
-  def __unicode__(self):
-    return '%s (%s)' % (self.name, self.category)
+    def __unicode__(self):
+        return f'{self.name}({self.category})'
