e02c5ae537f87edfab5b7ceb1f8f0fa5b04bd0d2
Daniele Guido <gui.daniele@gmail.com>
2020-06-05 18:13:01 +0200

    update serializers

--- miller/api/serializers/story.py
+++ miller/api/serializers/story.py
@@ -1,8 +1,42 @@
 from rest_framework import serializers
 from ...models import Story
+from .profile import UserSerializer
+from .author import AuthorSerializer
+from .document import LiteDocumentSerializer
+from .tag import TagSerializer
 
 
 class CreateStorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Story
         exclude = ('owner',)
+
+
+# Story Serializer to use in lists
+class BaseStorySerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True)
+    covers = LiteDocumentSerializer(many=True)
+    # source   = serializers.BooleanField(source='isSourceAvailable')
+
+    class Meta:
+        model = Story
+        fields = (
+            'id', 'slug', 'short_url', 'date',  'version',
+            'date_created',
+            'date_last_modified',
+            'status', 'covers', 'tags', 'data',
+            # 'source'
+        )
+
+
+# Story Serializer to use in lists
+class LiteStorySerializer(BaseStorySerializer):
+    authors = AuthorSerializer(many=True)
+    owner = UserSerializer()
+
+    class Meta:
+        model = Story
+        fields = (
+            'id', 'slug', 'short_url', 'date',
+            'date_created', 'date_last_modified', 'status', 'covers',
+            'authors', 'tags', 'owner', 'data')
