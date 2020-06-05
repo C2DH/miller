f17db2b782ff9056c8487331d0681d1378407d48
Daniele Guido <gui.daniele@gmail.com>
2020-06-05 15:34:58 +0200

    add contrib postgres app to settings

--- miller/settings.py
+++ miller/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     'oauth2_provider',
     'rest_framework',
     'miller',
