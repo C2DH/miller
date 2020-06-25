36622b908eacf2aaa928730d61bcc15dda3ee922
Daniele Guido <gui.daniele@gmail.com>
2020-06-25 09:13:09 +0200

    check attachment for every kind of document type

--- miller/admin.py
+++ miller/admin.py
@@ -104,13 +104,18 @@ class DocumentAdmin(admin.ModelAdmin):
     def thumbnail(self, instance):
         resolutions = instance.data.get(settings.MILLER_SIZES_SNAPSHOT_DATA_KEY, None)
         if not resolutions:
-            if instance.type not in ['image', 'pdf']:
-                return ''
-            if not instance.attachment or not getattr(instance.attachment, 'path', None):
-                return mark_safe('⚠️ missing <b>required</b> attachment')
-            if not os.path.exists(instance.attachment.path):
+            has_valid_attachment = instance.attachment and getattr(instance.attachment, 'path', None) and os.path.exists(instance.attachment.path)
+
+            if has_valid_attachment:
+                if instance.type in [Document.IMAGE, Document.PDF]:
+                    return mark_safe('... <b>ready to be queued</b> to get preview')
+                else:
+                    return f'attachment available, preview not available for type: {instance.type}'
+            if instance.url:
+                return mark_safe(f'remote url: <a href="{instance.url}">{instance.url}</a>, no preview available')
+            if instance.type in [Document.IMAGE, Document.AUDIO, Document.VIDEO, Document.AV, Document.PDF]:
                 return mark_safe('⚠️ attachment <b>not found</b>')
-            return mark_safe('... <b>ready to be queued</b> to get preview')
+            return ''
         thumbnail = resolutions.get('thumbnail', {})
         return mark_safe('<img src="{url}"  width="{width}" height="{height}" />'.format(**thumbnail))
 
