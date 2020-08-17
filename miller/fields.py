c52555346b162e3e2381e6c7599568a11941eab2
Daniele Guido <gui.daniele@gmail.com>
2020-08-17 18:20:54 +0200

    Upgrade/django (#46)
    * upgrade packages to django 3.1
    
    * use default class from django.db.models (new in django 3.1)
    
    * bump version

--- miller/fields.py
+++ miller/fields.py
@@ -1,26 +1,9 @@
-import json
-from django.contrib.postgres.fields import JSONField
-from django.contrib.postgres.forms.jsonb import (
-    InvalidJSONInput,
-    JSONField as JSONFormField,
-)
-
-
-class UTF8JSONFormField(JSONFormField):
-    def prepare_value(self, value):
-        if isinstance(value, InvalidJSONInput):
-            return value
-        return json.dumps(value, ensure_ascii=False)
-
+from django.db.models import JSONField
 
 class UTF8JSONField(JSONField):
     """JSONField for postgres databases.
-
+    Deprecated.
     Displays UTF-8 characters directly in the admin, i.e. äöü instead of
     unicode escape sequences.
     """
-    def formfield(self, **kwargs):
-        return super().formfield(**{
-            **{'form_class': UTF8JSONFormField},
-            **kwargs,
-        })
+    pass
