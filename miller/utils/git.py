fa7a3ca41a36e7a85007de13f1bc50b3e3304765
Daniele Guido <gui.daniele@gmail.com>
2020-08-18 11:21:24 +0200

    add commit_instance helper (for every model that can be serialized)

--- miller/utils/git.py
+++ miller/utils/git.py
@@ -1,6 +1,10 @@
 import os
-from git import Repo, Commit, Actor, Tree
+import logging
+from git import Repo, Actor
 from django.conf import settings
+from django.core import serializers
+
+logger = logging.getLogger(__name__)
 
 def get_repo():
     if not settings.MILLER_CONTENTS_ENABLE_GIT:
@@ -8,7 +12,7 @@ def get_repo():
     if not settings.MILLER_CONTENTS_ROOT:
         raise NameError('You shoud set MILLER_CONTENTS_ROOT to an absolute path in the settings file')
     if not os.path.exists(settings.MILLER_CONTENTS_ROOT):
-        raise OsError(f'path for MILLER_CONTENTS_ROOT does not exist or it is not reachable: {settings.MILLER_CONTENTS_ROOT}')
+        raise OsError(f'path for MILLER_CONTENTS_ROOT does not exist or it is not reachable: {settings.MILLER_CONTENTS_ROOT}')  # noqa: F821
     repo = Repo.init(settings.MILLER_CONTENTS_ROOT)
     return repo
 
@@ -31,3 +35,27 @@ def commit_filepath(filepath, username, email, message):
     commit_message = repo.index.commit(message=message, author=author, committer=committer)
     short_sha = repo.git.rev_parse(commit_message, short=7)
     return short_sha
+
+
+def commit_instance(
+    instance,
+    verbose=False,
+    serializer='yaml'
+):
+    logger.info(
+        f'commit_instance for instance pk:{instance.pk} model:{instance._meta.model.__name__}'
+    )
+    folder_path = get_or_create_path_in_contents_root(folder_path=instance._meta.model.__name__)
+    contents = serializers.serialize(serializer, [instance])
+    filepath = os.path.join(folder_path, f'{instance.pk}-{instance.short_url}.yml')
+    with open(filepath, 'w', encoding='utf-8') as f:
+        f.write(contents)
+        logger.info(f'commit_instance document: {instance.pk} {instance.short_url} written to {filepath}')
+    if settings.MILLER_CONTENTS_ENABLE_GIT:
+        short_sha = commit_filepath(
+            filepath=filepath,
+            username=instance.owner if instance.owner is not None else settings.MILLER_CONTENTS_GIT_USERNAME,
+            email=settings.MILLER_CONTENTS_GIT_EMAIL,
+            message=f'saving {instance.title}'
+        )
+        logger.info(f'commit_instance document: {instance.pk} {instance.short_url} committed: {short_sha}')
