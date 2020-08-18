0520872057c8092cb4360b45694b714b6e72dd00
Daniele Guido <gui.daniele@gmail.com>
2020-08-18 11:19:52 +0200

    add commands for stories and documents

--- miller/management/commands/story_commit.py
+++ miller/management/commands/story_commit.py
@@ -1,21 +1,21 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from miller.models import Document
-from miller.tasks import commit_document
+from miller.models import Story
+from miller.tasks import commit_story
 
 class Command(BaseCommand):
     """
     usage:
-    ENV=development pipenv run ./manage.py document_commit <pks>
+    ENV=development pipenv run ./manage.py story_commit <pks>
     or if in docker:
     docker exec -it docker_miller_1 \
-    python manage.py document_commit \
-    <document_pk>, <document_pk> ... [--immediate] [--verbose]
+    python manage.py story_commit \
+    <story_pk>, <story_pk> ... [--immediate] [--verbose]
     """
     help = 'save YAML version of documents and commit them (if a git repo is enaled)'
 
     def add_arguments(self, parser):
-        parser.add_argument('document_pks', nargs='+', type=int)
+        parser.add_argument('story_pks', nargs='+', type=int)
         parser.add_argument(
             '--immediate',
             action='store_true',
@@ -28,19 +28,19 @@ class Command(BaseCommand):
         )
 
     def handle(
-        self, document_pks, immediate=False, verbose=False, *args, **options
+        self, story_pks, immediate=False, verbose=False, *args, **options
     ):
         if not settings.MILLER_CONTENTS_ENABLE_GIT:
             self.stderr.write('MILLER_CONTENTS_ENABLE_GIT not enabled!')
         # repo = get_repo()
-        self.stdout.write(f'document_commit for: {document_pks}')
-        docs = Document.objects.filter(pk__in=document_pks)
+        self.stdout.write(f'story_commit for: {story_pks}')
+        docs = Story.objects.filter(pk__in=story_pks)
         # loop through documents
         for doc in docs:
             try:
                 if immediate:
                     doc.commit()
                 else:
-                    commit_document.delay(document_pk=doc.pk)
+                    commit_story.delay(story_pk=doc.pk)
             except Exception as e:
                 self.stderr.write(e)
