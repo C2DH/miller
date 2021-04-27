baac48d27fa924fee59ac282a91bf2c335f331d8
Daniele Guido <gui.daniele@gmail.com>
2021-04-27 14:41:54 +0200

    Patch/watchdog (#50)
    * add watchmedo on entrypoint in order to handle static files changes (that is, for the JSON schema)
    * add commands to be executed after 'watchmedo'
    * quick fix: add author slug in the admin

--- miller/models/author.py
+++ miller/models/author.py
@@ -25,3 +25,6 @@ class Author(models.Model):
     user = models.ForeignKey(
         User, related_name='authorship', blank=True,
         null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.slug
