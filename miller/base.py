698d54fb8fbcb229f99908a875a79cacbb05cf81
Daniele Guido <gui.daniele@gmail.com>
2021-12-20 11:54:22 +0100

    Update django and use dotenv package to handle .env files

--- miller/base.py
+++ miller/base.py
@@ -7,30 +7,32 @@ from pathlib import Path  # python3 only
 
 logger = logging.getLogger(__name__)
 
+from dotenv import dotenv_values
 
-def dotenv_values(dotenv_path):
-    lines = []
-    try:
-        with open(dotenv_path) as fp:
-            lines = fp.read().splitlines()
-    except FileNotFoundError as e:
-        if sys.argv[1] == 'test':
-            logger.warning(
-                f'No dotenv file found using dotenv_path:{dotenv_path}'
-            )
-            return {}
-        else:
-            raise e
-
-    # get tuples of values,property splitting each line of the file
-    lines = map(lambda l: tuple(re.split(r'\s*=\s*', l, 1)), filter(
-        None, lines
-    ))
-    lines = list(lines)
-    print(f"dotenv_values: found {len(lines)} valid lines")
-    if not lines:
-        return dict()
-    return dict(lines)
+#
+# def deprecated_dotenv_values(dotenv_path):
+#     lines = []
+#     try:
+#         with open(dotenv_path) as fp:
+#             lines = fp.read().splitlines()
+#     except FileNotFoundError as e:
+#         if sys.argv[1] == 'test':
+#             logger.warning(
+#                 f'No dotenv file found using dotenv_path:{dotenv_path}'
+#             )
+#             return {}
+#         else:
+#             raise e
+#
+#     # get tuples of values,property splitting each line of the file
+#     lines = map(lambda l: tuple(re.split(r'\s*=\s*', l, 1)), filter(
+#         None, lines
+#     ))
+#     lines = list(lines)
+#     print(f"dotenv_values: found {len(lines)} valid lines")
+#     if not lines:
+#         return dict()
+#     return dict(lines)
 
 
 def get_env_variable(var_name, default=None):
@@ -52,4 +54,4 @@ dotenv_filename = '.{0}.env'.format(
 dotenv_path = str(Path('.') / dotenv_filename)
 dotenv_dict = dotenv_values(dotenv_path=dotenv_path)
 
-print('loading env file: {0}'.format(dotenv_filename))
+logger.debug(f'loading env file: {dotenv_filename}')
