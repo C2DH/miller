d31d44c4b2a12a48c720a9db39be90ab4d6f8d10
Daniele Guido <1181642+danieleguido@users.noreply.github.com>
2024-11-27 11:41:36 +0100

    Update init_git.py

--- miller/management/commands/init_git.py
+++ miller/management/commands/init_git.py
@@ -9,47 +9,53 @@ class Command(BaseCommand):
     docker exec -it docker_miller_1 \
         python manage.py init_git
     """
-    help = '''
+
+    help = """
         Initialize git in the path specified
         in settings.MILLER_CONTENTS_ROOT
-    '''
+    """
 
     def handle(self, *args, **options):
         if not settings.MILLER_CONTENTS_ENABLE_GIT:
-            self.stderr.write('MILLER_CONTENTS_ENABLE_GIT not enabled!')
+            self.stderr.write("MILLER_CONTENTS_ENABLE_GIT not enabled!")
             return
-        # repo is the new Repo
-        repo = Repo.init(settings.MILLER_CONTENTS_ROOT)
-        print(repo)
-        for prefix in ['story', 'document']:
-            prefixed_path = os.path.join(
-                settings.MILLER_CONTENTS_ROOT,
-                prefix
+        try:
+            # repo is the new Repo
+            repo = Repo.init(settings.MILLER_CONTENTS_ROOT)
+            repo.git.config(
+                "--global", "--add", "safe.directory", settings.MILLER_CONTENTS_ROOT
             )
+        except Exception as e:
+            self.stderr.write(f"Error: {e}")
+            return
+        print(repo)
+        for prefix in ["story", "document"]:
+            prefixed_path = os.path.join(settings.MILLER_CONTENTS_ROOT, prefix)
             if not os.path.exists(prefixed_path):
                 os.makedirs(prefixed_path)
-                self.stdout.write(f'creating path: {prefixed_path}')
+                self.stdout.write(f"creating path: {prefixed_path}")
             else:
-                self.stdout.write(f'path exists: {prefixed_path}')
+                self.stdout.write(f"path exists: {prefixed_path}")
             # create .gitkeep
-            with open(os.path.join(prefixed_path, '.gitkeep'), 'w'):
+            with open(os.path.join(prefixed_path, ".gitkeep"), "w"):
                 pass
         # add gitignore
         self.stdout.write('add ".gitignore" ...')
-        with open(os.path.join(
-            settings.MILLER_CONTENTS_ROOT,
-            '.gitignore'
-        ), 'w') as file:
-            file.write('.DS_Store')
-            self.stdout.write('.gitignore added')
+        with open(
+            os.path.join(settings.MILLER_CONTENTS_ROOT, ".gitignore"), "w"
+        ) as file:
+            file.write(".DS_Store")
+            self.stdout.write(".gitignore added")
+        try:
+            # get untracked files
+            self.stdout.write(f"add untracked files: {repo.untracked_files}...")
 
-        # get untracked files
-        self.stdout.write(f'add untracked files: {repo.untracked_files}...')
-
-        # add untracked files
-        if len(repo.untracked_files) > 0:
-            repo.index.add(repo.untracked_files)
-            repo.index.commit("Fresh start.")
-            self.stdout.write('untracked files added and committed.')
-
-        self.stdout.write('nothing to add, git ready')
+            # add untracked files
+            if len(repo.untracked_files) > 0:
+                repo.index.add(repo.untracked_files)
+                repo.index.commit("Fresh start.")
+                self.stdout.write("untracked files added and committed.")
+        except Exception as e:
+            self.stderr.write(f"Error: {e}")
+            return
+        self.stdout.write("nothing to add, git ready")
